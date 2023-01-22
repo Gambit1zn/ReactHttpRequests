cf009be43462e495837aaf92410c58db3a30de39
Ethan Ankadu <ethanankadu1zn@gmail.com>
2023-01-22 10:58:50 +0200

    Adding loading state and no movies fallback

--- src/App.js
+++ src/App.js
@@ -5,8 +5,10 @@ import './App.css';
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   function fetchMoviesHandler() {
+    setIsLoading(true);
     fetch('https://swapi.dev/api/films/').then(response => {
        return response.json();
     }).then(data => {
@@ -18,8 +20,8 @@ function App() {
           releaseDate: movie.release_date
         };
       });
-
       setMovies(transformedMovies);
+      setIsLoading(false);
     });
   }
 
@@ -29,7 +31,9 @@ function App() {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
+        {!isLoading && movies.length === 0 && <p>Found no movies.</p>}
+        {isLoading && <p>Loading...</p>}
       </section>
     </React.Fragment>
   );
